ae9eb967c8a83fc31445763dc10b267d8916074f
pixobe <email@pixobe.com>
2024-07-27 07:35:20 +0800

    init commit

---
+++ assets/Blueprint.stories-CL-Ah9a_.js
@@ -0,0 +1,7 @@
+const l={title:"BluePrint",render:i=>{const s=document.createElement("blue-print");return Object.assign(s,i),s},argTypes:{},args:{}},r={args:{}},e={args:{model:"models/dining-room.glb"}};var o,a,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {}
+}`,...(t=(a=r.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var n,c,d;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    model: 'models/dining-room.glb'
+  }
+}`,...(d=(c=e.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const m=["Primary","BuiltModel"];export{e as BuiltModel,r as Primary,m as __namedExportsOrder,l as default};
